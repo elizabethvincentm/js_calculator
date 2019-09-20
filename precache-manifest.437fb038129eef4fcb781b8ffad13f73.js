55fe212e0af97f60d6b5826832212642290c1d2c
Elizabeth Vincent M <elizabethvincentm@gmail.com>
2019-09-20 17:24:47 +0100

    Updates

---
+++ precache-manifest.437fb038129eef4fcb781b8ffad13f73.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d753c248ffa58a9edd92bb37f79563cb",
+    "url": "/js_calculator/index.html"
+  },
+  {
+    "revision": "e4d271359cdbcb705b5c",
+    "url": "/js_calculator/static/css/main.5211b182.chunk.css"
+  },
+  {
+    "revision": "f10eb1de7f414c18bc36",
+    "url": "/js_calculator/static/js/2.891ce21f.chunk.js"
+  },
+  {
+    "revision": "e4d271359cdbcb705b5c",
+    "url": "/js_calculator/static/js/main.9a679295.chunk.js"
+  },
+  {
+    "revision": "13911a41ae04e096991b",
+    "url": "/js_calculator/static/js/runtime~main.90d3e16e.js"
+  }
+]);
